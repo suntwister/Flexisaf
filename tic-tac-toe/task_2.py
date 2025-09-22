b35d65e4d4d12d10c1edaf3e3a69fd5d0dc4fc2f
Samuel Oyewusi <s.suntwister@gmail.com>
2025-09-22 04:55:07 +0100

    added exer 2

--- tic-tac-toe/task_2.py
+++ tic-tac-toe/task_2.py
@@ -55,3 +55,32 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+# --------------------- Exericise 2 ----------------
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load CSV file
+data = pd.read_csv("sales_plot\company_sales_data.csv")
+
+# Line plot for total profit
+plt.plot(data['month_number'], data['total_profit'], marker='o')
+plt.title("Company Profit per Month")
+plt.xlabel("Month")
+plt.ylabel("Total Profit")
+plt.show()
+
+
+# # Subplots for Bathing soap and Facewash
+plt.subplot(2, 1, 1)   # 2 rows, 1 column, first plot
+plt.plot(data['month_number'], data['bathingsoap'], marker='o')
+plt.title("Bathing Soap Sales")
+
+plt.subplot(2, 1, 2)   # second plot
+plt.plot(data['month_number'], data['facewash'], marker='o')
+plt.title("Facewash Sales")
+
+plt.tight_layout()
+plt.show()
